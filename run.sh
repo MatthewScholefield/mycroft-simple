1e9534e6928919cb48bffed6566a7465f46e3c82
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-08-01 18:08:49 -0500

    Improve run.sh Auto install fann if not in package manager and retry pip install -e . if installing packages. Also save packages to file to trigger on new dependencies

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+apt_packages="python3-pip portaudio19-dev python3-dev swig virtualenv libyaml-dev libpulse-dev vlc libfann-dev"
+pip_packages="$(cat setup.py | tr -d '\n' | tr -d ' ' | grep -Eo 'install_requires=\[[^]]*\]')"
+
 found_exe() {
     hash "$1" 2>/dev/null
+}
+
+found_lib() {
+    IFS=:
+    found=1
+
+    for p in ${LD_LIBRARY_PATH}; do
+        if [ -e ${p}/$1 ]; then
+            found=0
+        fi
+    done
+    IFS=$' '
+    return $found
 }
 
 check_no_root() {
@@ -11,11 +27,28 @@ check_no_root() {
     fi
 }
 
+install_fann() {
+    echo "Compiling FANN..."
+    if ! found_exe cmake; then
+        echo "Please install cmake first."
+        exit 1
+    fi
+    rm -rf /var/tmp/fann-2.2.0
+    curl -L https://github.com/libfann/fann/archive/2.2.0.tar.gz | tar xvz -C /var/tmp
+    pushd /var/tmp/fann-2.2.0
+    cmake .
+    sudo make install
+    popd
+}
+
 install_deps() {
     if found_exe apt-get; then
-        apt_packages="python3-pip portaudio19-dev python3-dev swig virtualenv libyaml-dev libpulse-dev vlc libfann-dev"
         echo "Installing $apt_packages..."
         sudo apt-get install -y $apt_packages
+        fann_version=$(dpkg -s libfann-dev 2>/dev/null | grep Version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+        if [ $fann_version == 0.2.2 ]; then
+            sudo apt-get remove -y libfann-dev
+        fi
     else
         if found_exe tput; then
 			green="$(tput setaf 2)"
@@ -27,14 +60,17 @@ install_deps() {
         echo "${green}Make sure to manually install:${blue} $apt_packages"
         echo $reset
     fi
+    if ! found_lib libdoublefann.so; then
+        install_fann /var/tmp/fann
+    fi
 }
 
 installed_deps() {
-    [ -f ".installed_deps" ]
+    [ -f ".installed_deps" ] && [ "$(cat .installed_deps)" == "$apt_packages $pip_packages" ]
 }
 
 mark_complete() {
-    touch .installed_deps
+    echo "$apt_packages $pip_packages" > .installed_deps
 }
 
 update_param() {
@@ -77,7 +113,7 @@ create_virtualenv
 
 activate_virtualenv
 
-if ! found_exe mycroft_simple; then
+if ! found_exe mycroft_simple || ! installed_deps; then
     pip3 install -e .
 fi
 
